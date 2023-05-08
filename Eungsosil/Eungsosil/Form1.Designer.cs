497996ef05e312f817b609c40fbd68edc17198f2
LSH-kw0315 <tmdgjsdl1234@naver.com>
2023-05-09 00:57:31 +0900

    크롤링 동작을 테스트함. 기존에 webBrowser 컨트롤을 이용하는 방식은 이 프로젝트의 프레임워크가 지원하지 않아서 크롤링을 하기 위해서는 Nugget 패키지 관리자에서 WebView2를 설치해주시기 바랍니다.

--- Eungsosil/Eungsosil/Form1.Designer.cs
+++ Eungsosil/Eungsosil/Form1.Designer.cs
@@ -28,12 +28,50 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            web_view = new Microsoft.Web.WebView2.WinForms.WebView2();
+            textBox1 = new TextBox();
+            ((System.ComponentModel.ISupportInitialize)web_view).BeginInit();
+            SuspendLayout();
+            // 
+            // web_view
+            // 
+            web_view.AllowExternalDrop = true;
+            web_view.CreationProperties = null;
+            web_view.DefaultBackgroundColor = Color.White;
+            web_view.Location = new Point(73, 12);
+            web_view.Name = "web_view";
+            web_view.Size = new Size(650, 355);
+            web_view.TabIndex = 0;
+            web_view.ZoomFactor = 1D;
+            web_view.NavigationCompleted += web_view_NavigationCompleted;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(53, 373);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.ScrollBars = ScrollBars.Both;
+            textBox1.Size = new Size(685, 148);
+            textBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(9F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(textBox1);
+            Controls.Add(web_view);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)web_view).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Microsoft.Web.WebView2.WinForms.WebView2 web_view;
+        private TextBox textBox1;
     }
 }
--- Eungsosil/Eungsosil/Form1.cs
+++ Eungsosil/Eungsosil/Form1.cs
@@ -1,10 +1,83 @@
+
+using System.Linq.Expressions;
+
 namespace Eungsosil
 {
     public partial class Form1 : Form
     {
+        Dictionary<string, string> course_grade;
+        double total_grade;
+
         public Form1()
         {
             InitializeComponent();
+            course_grade = new Dictionary<string, string>();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            web_view.Source = new Uri("https://klas.kw.ac.kr");
+        }
+
+        private async void web_view_NavigationCompleted(object sender, Microsoft.Web.WebView2.Core.CoreWebView2NavigationCompletedEventArgs e)
+        {
+            if (web_view.Source.ToString() == "https://klas.kw.ac.kr/std/cmn/frame/Frame.do")
+            {
+
+                web_view.Source = new Uri("https://klas.kw.ac.kr/std/cps/inqire/AtnlcScreStdPage.do");
+            }
+            if (web_view.Source.ToString() == "https://klas.kw.ac.kr/std/cps/inqire/AtnlcScreStdPage.do")
+            {
+
+                var res = await web_view.ExecuteScriptAsync("document.getElementsByClassName('tablegw')[2].getElementsByTagName('td')[13].innerText;");
+                res=res.Replace('"', ' ').Trim();
+                try
+                {
+                    total_grade = Double.Parse(res);
+                }
+                catch
+                {
+                    //비동기적 처리를 하기 때문에 오류 메시지 발생하지만 결과적으로는 정상 작동한다. 일부러 비워두었다.
+                }
+                res = await web_view.ExecuteScriptAsync("document.getElementsByClassName('AType').length");
+                int atype_amount = Convert.ToInt32(res) / 2;
+
+                for (int i = 0; i < atype_amount; i++)
+                {
+                    res = await web_view.ExecuteScriptAsync(String.Format("document.getElementsByClassName('AType')[{0}].getElementsByTagName('tbody')[0].getElementsByTagName('tr').length", i));
+                    int tr_amount = Convert.ToInt32(res);
+                    for (int j = 0; j < tr_amount; j++)
+                    {
+                        res = await web_view.ExecuteScriptAsync(String.Format("document.getElementsByClassName('AType')[{0}].getElementsByTagName('tbody')[0].getElementsByTagName('tr')[{1}].getElementsByTagName('td')[1].innerText", i, j));
+                        string key = res.Replace('"', ' ').Trim();
+                        res = await web_view.ExecuteScriptAsync(String.Format("document.getElementsByClassName('AType')[{0}].getElementsByTagName('tbody')[0].getElementsByTagName('tr')[{1}].getElementsByTagName('td')[5].innerText", i, j));
+                        string value = res.Replace('"', ' ').Trim();
+                        if (!String.IsNullOrEmpty(key) && !String.IsNullOrEmpty(value))
+                        {
+                            try
+                            {
+                                course_grade.Add(key, value);
+                            }
+                            catch (Exception)
+                            {
+                                continue;
+                            }
+                        }
+
+
+
+                    }
+                }
+
+                textBox1.Text = "총 평점:" + total_grade.ToString()+"\r\n";
+                foreach (var item in course_grade)
+                {
+                    textBox1.Text += item.Key + ":" + item.Value + "\r\n===\r\n";
+                }
+
+
+
+            }
         }
     }
 }
